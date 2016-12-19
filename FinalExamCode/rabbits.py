f5ee6b0adbf57cb690586151d0a07819fac224fb
Stan Kolis <theman0605@gmail.com>
2016-12-18 19:40:40 -0500

    Finished P6, Added rabbits

---
+++ FinalExamCode/rabbits.py
@@ -0,0 +1,86 @@
+import random
+import pylab
+
+# Global Variables
+MAXRABBITPOP = 1000
+CURRENTRABBITPOP = 500
+CURRENTFOXPOP = 30
+
+def rabbitGrowth():
+    """ 
+    rabbitGrowth is called once at the beginning of each time step.
+
+    It makes use of the global variables: CURRENTRABBITPOP and MAXRABBITPOP.
+
+    The global variable CURRENTRABBITPOP is modified by this procedure.
+
+    For each rabbit, based on the probabilities in the problem set write-up, 
+      a new rabbit may be born.
+    Nothing is returned.
+    """
+    # you need this line for modifying global variables
+    global CURRENTRABBITPOP
+    increase = 0
+    rabbitList = range(CURRENTRABBITPOP)
+    for rabbit in rabbitList:    
+        if random.random() < 1 - (CURRENTRABBITPOP/MAXRABBITPOP):
+            increase += 1
+    CURRENTRABBITPOP += increase
+            
+def foxGrowth():
+    """ 
+    foxGrowth is called once at the end of each time step.
+
+    It makes use of the global variables: CURRENTFOXPOP and CURRENTRABBITPOP,
+        and both may be modified by this procedure.
+
+    Each fox, based on the probabilities in the problem statement, may eat 
+      one rabbit (but only if there are more than 10 rabbits).
+
+    If it eats a rabbit, then with a 1/3 prob it gives birth to a new fox.
+
+    If it does not eat a rabbit, then with a 1/10 prob it dies.
+
+    Nothing is returned.
+    """
+    # you need these lines for modifying global variables
+    global CURRENTRABBITPOP
+    global CURRENTFOXPOP
+
+    # TO DO
+    foxList = range(CURRENTFOXPOP)
+    increase = 0    
+    for fox in foxList:
+        while CURRENTRABBITPOP > 10:        
+            if random.random() < CURRENTRABBITPOP/MAXRABBITPOP:
+                CURRENTRABBITPOP -= 1
+            if random.random() < 1/3:
+                increase += 1
+            else:
+                if random.random() < 0.10 and CURRENTFOXPOP > 10:
+                    increase -= 1
+    CURRENTFOXPOP += increase
+    
+    
+            
+def runSimulation(numSteps):
+    """
+    Runs the simulation for `numSteps` time steps.
+
+    Returns a tuple of two lists: (rabbit_populations, fox_populations)
+      where rabbit_populations is a record of the rabbit population at the 
+      END of each time step, and fox_populations is a record of the fox population
+      at the END of each time step.
+
+    Both lists should be `numSteps` items long.
+    """
+
+    rabbits = []
+    foxes = []
+    for i in range(numSteps):
+        rabbits.append(rabbitGrowth())
+        foxes.append(foxGrowth())
+    pylab.plot(range(numSteps), rabbits, label="Rabbits")
+    pylab.plot(range(numSteps), foxes, label="Foxes")
+    pylab.show()
+    return(rabbits,foxes)
